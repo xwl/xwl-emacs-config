fca0944e9cce6b2e346c659ae62f022f3c3b701d
William Xu <william.xwl@gmail.com>
2010-01-20 23:34:19 +0800

    (xwl-erc-hook): Call erc-match-current-nick-p to check.

--- .emacs.d/site-lisp/config/xwl-erc.el
+++ .emacs.d/site-lisp/config/xwl-erc.el
@@ -377,6 +377,35 @@ so as to keep an eye on work when necessarily."
   (global-set-key (kbd "C-c RET") (kbd "C-c C-@")))
 
 
+;; ,----
+;; | growl
+;; `----
+
+(when (eq system-type 'darwin)
+
+  (setq growlnotify-command (executable-find "growlnotify"))
+
+  (defun xwl-growl (title message)
+    (start-process "growl" " growl"
+                   growlnotify-command
+                   title
+                   "-a" "Emacs")
+    (process-send-string " growl" message)
+    (process-send-string " growl" "\n")
+    (process-send-eof " growl"))
+
+  (defun xwl-erc-hook (match-type nickuserhost message)
+    "Shows a growl notification, when user's nick was mentioned.
+If the buffer is currently not visible, makes it sticky."
+    (when (erc-match-current-nick-p nickuserhost msg)
+      (xwl-growl
+       (concat "ERC: name mentioned on: " (buffer-name (current-buffer)))
+       message)))
+
+  (add-hook 'erc-text-matched-hook 'xwl-erc-hook)
+
+  )
+
 (provide 'xwl-erc)
 
 ;;; xwl-erc.el ends here
