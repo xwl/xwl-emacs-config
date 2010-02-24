c18125266d55e43135e2e42a10fdc72098e88e8e
William Xu <william.xwl@gmail.com>
2010-02-25 00:28:35 +1100

    xwl-erc.el: Cleanup, insert date stamp by day.

--- .emacs.d/site-lisp/config/xwl-erc.el
+++ .emacs.d/site-lisp/config/xwl-erc.el
@@ -48,7 +48,15 @@
 (setq erc-common-server-suffixes nil
       erc-mode-line-format "%t %a")
 
-;; autojoin
+(when (xwl-tty-p)
+  (global-set-key (kbd "C-c RET") (kbd "C-c C-@")))
+
+(setq erc-join-buffer 'bury
+      erc-auto-query 'bury)
+
+;; ,----
+;; | autojoin, identify, op
+;; ----
 
 (erc-autojoin-mode 1)
 (setq erc-autojoin-channels-alist
@@ -85,10 +93,39 @@
 
         ))
 
-(setq erc-join-buffer 'bury
-      erc-auto-query 'bury)
+(defun xwl-erc-auto-identify (server nick)
+  (unless (string-match "localhost" server) ; bitlbee
+    (erc-message "PRIVMSG"
+                 (format "NickServ identify %s" pwbitlbee))))
 
-;; match & track
+(add-hook 'erc-after-connect 'xwl-erc-auto-identify)
+
+(defun his-bitlbee-identify ()
+   "If we're on the bitlbee server, send the identify command to the
+ &bitlbee channel."
+   (when (and (string= "localhost" erc-session-server)
+              (string= "&bitlbee" (buffer-name)))
+     (erc-message "PRIVMSG" (format "%s identify %s"
+                                    (erc-default-target)
+                                    pwbitlbee))))
+
+(add-hook 'erc-join-hook 'his-bitlbee-identify)
+
+(defun xwl-erc-auto-op ()
+  (let ((b (buffer-name)))
+    (cond
+     ((string= b "#emacs-cn")
+      (erc-message "PRIVMSG" (concat "chanserv op " b)))
+     ((string= b "#avkon")
+      (erc-message "PRIVMSG" (concat "userserv login xwl " pwerc))
+      (erc-message "PRIVMSG" (concat "chanserv op " b))))))
+
+(add-hook 'erc-join-hook 'xwl-erc-auto-op)
+
+;; ,----
+;; | match, ignore
+;; `----
+
 (require 'erc-match)
 (erc-match-mode 1)
 (setq erc-current-nick-highlight-type 'nick-or-keyword)
@@ -98,22 +135,6 @@
 (defface erc-query-buffer-face '((t (:bold t :foreground "magenta")))
   "ERC face for your query buffers."
   :group 'erc-faces)
-
-;; erc-join-hook
-;; (defun xwl-erc-join-hook ()
-;;   (erc-match-mode 1)
-
-;; (global-set-key (kbd "C-c C-2") 'erc-track-switch-buffer)
-
-(setq erc-track-faces-priority-list
-      '(erc-query-buffer-face
-        erc-current-nick-face
-	erc-keyword-face
-	erc-pal-face
-	erc-default-face
-	))
-
-(setq erc-track-priority-faces-only 'all)
 
 (defun xwl-toggle-erc-busy ()
   "Toggle `erc-default-face' in `erc-track-faces-priority-list'
@@ -132,106 +153,70 @@ so as to keep an eye on work when necessarily."
 
 (global-set-key (kbd "C-c n e") 'xwl-toggle-erc-busy)
 
+(setq erc-ignore-list nil)
+(setq erc-hide-list
+      '("JOIN" "PART" "QUIT" "MODE"))
+
+;; ,----
+;; | track, fill
+;; `----
+
 (require 'erc-track)
 
 (setq erc-track-switch-direction 'importance)
 
-;; (defun xwl-erc-track-switch-buffer-by-face-priority ()
-;;   "Run `erc-track-switch-buffer' but by `erc-track-faces-priority-list'."
-;;   (interactive)
-;;   (when (and erc-track-mode erc-modified-channels-alist)
-;;     (let ((buf nil)                     ; buf to switch to firstly
-;;           (faces erc-track-faces-priority-list)
-;;           (channels-1 erc-modified-channels-alist)
-;;           (channels erc-modified-channels-alist)
-;;           (f nil)
-;;           (c nil))
-;;       (while faces
-;;         (setq f (car faces))
-;;         (setq channels channels-1)
-;;         (while channels
-;;           (setq c (car channels))
-;;           (if (eq (cddr c) f)
-;;               (setq channels nil
-;;                     faces nil
-;;                     buf (car c))
-;;             (setq channels (cdr channels))))
-;;         (setq faces (cdr faces)))
-;;       (if buf
-;;           (switch-to-buffer buf)
-;;         (call-interactively 'erc-track-switch-buffer)))))
+(setq erc-track-faces-priority-list
+      '(erc-query-buffer-face
+        erc-current-nick-face
+	erc-keyword-face
+	erc-pal-face
+	erc-default-face
+	))
 
-;; (global-set-key (kbd "C-c C-@") 'xwl-erc-track-switch-buffer-by-face-priority)
-;; (global-set-key (kbd "C-c RET") 'xwl-erc-track-switch-buffer-by-face-priority)
+(setq erc-track-priority-faces-only 'all)
 
 (defadvice erc-track-switch-buffer (before place-point-to-bottom activate)
   (when (memq (current-buffer) (erc-buffer-list))
     (goto-char (point-max))
     (forward-line -1)))
 
-;; fill
 (require 'erc-fill)
 (erc-fill-mode 1)
 (setq erc-fill-function 'erc-fill-static
       erc-fill-static-center 10
       erc-fill-prefix "      ")
 
-;; timestamp
+;; ,----
+;; | timestamp
+;; `----
+
 (require 'erc-stamp)
 (erc-timestamp-mode 1)
 
-(setq erc-timestamp-only-if-changed-flag nil
+(setq erc-timestamp-only-if-changed-flag t
       erc-timestamp-format "%H:%M ")
 
-;; (setq xwl-erc-timestamp-last nil)
-
-;; (load "~/.erc/.timestamp-last.el")
-
-;; (defun xwl-erc-insert-timestamp-left-by-day (string)
-;;   "If last timestamp is one or more "
-;;   (if (or (null xwl-erc-timestamp-last)  ; nil
-;;           (> (- (time-to-days (current-time)) ; >=1 days
-;;                 (time-to-days xwl-erc-timestamp-last))
-;;              0)
-;;           (< (string-to-number (format-time-string "%H")) ; 18-00-03
-;;              (string-to-number
-;;               (format-time-string "%H" xwl-erc-timestamp-last))))
-;;       (progn
-;;         (setq xwl-erc-timestamp-last (current-time))
-;;         (erc-insert-timestamp-left
-;;          (erc-format-timestamp
-;;           (current-time) (concat "%Y.%m.%d " erc-timestamp-format)))
-;;         (let ((buf (find-file-noselect "~/.erc/.timestamp-last.el")))
-;;           (with-current-buffer buf
-;;             (let ((inhibit-read-only t))
-;;               (kill-region (point-min) (point-max))
-;;               (insert (format "(setq xwl-erc-timestamp-last '%s)\n"
-;;                               (current-time)))
-;;               (save-buffer)))
-;;           (kill-buffer buf)))
-;;     (erc-insert-timestamp-left
-;;      (erc-format-timestamp (current-time) erc-timestamp-format))))
-
 (setq erc-insert-timestamp-function
-      ;; 'xwl-erc-insert-timestamp-left-by-day)
-      'erc-insert-timestamp-left)
+      ;; 'erc-insert-timestamp-left
+      'ks-timestamp)
 
-;; spelling, TODO, fix this.
-;; (require 'erc-spelling)
-;; (erc-spelling-mode -1)
+(setq xwl-erc-datestamp-format " === [%a(%V) %Y/%m/%d] ===\n")
 
-;; ignore
-(setq erc-ignore-list nil)
-(setq erc-hide-list 
-      '("JOIN" "PART" "QUIT" "MODE"))
+(defvar xwl-erc-last-datestamp nil)
+(make-variable-buffer-local 'xwl-erc-last-datestamp)
 
-;; ;; sound
-;; (erc-sound-enable)
-;; (setq erc-sound-path "/home/william/music/sound")
-;; (setq erc-default-sound "/home/william/music/sound/reflection.mp4")
-;; (setq erc-play-command "mplayer")
+(defun ks-timestamp (string)
+  (erc-insert-timestamp-left string)
+  (let ((datestamp (erc-format-timestamp (current-time)
+                                         xwl-erc-datestamp-format)))
+    (unless (string= datestamp xwl-erc-last-datestamp)
+      (erc-insert-timestamp-left datestamp)
+      (setq xwl-erc-last-datestamp datestamp))))
 
-;; log
+;; ,----
+;; | log
+;; `----
+
 (require 'erc-log)
 (erc-log-mode 1)
 (setq erc-log-channels-directory "~/var/erc/"
@@ -243,13 +228,49 @@ so as to keep an eye on work when necessarily."
 (unless (file-exists-p erc-log-channels-directory)
   (mkdir erc-log-channels-directory t))
 
-;; FIXME
-;; bbdb
-;; (require 'erc-bbdb)
-;; (erc-bbdb-mode 1)
-;; (setq erc-bbdb-popup-type nil)
+;; ,----
+;; | notify
+;; `----
 
-;; goodies
+(setq growlnotify-command (executable-find "growlnotify"))
+
+(defun xwl-growl (title message)
+  (start-process "growl" " growl" growlnotify-command title "-a" "Emacs")
+  (process-send-string " growl" message)
+  (process-send-string " growl" "\n")
+  (process-send-eof " growl"))
+
+(defun xwl-erc-text-matched-hook (match-type nickuserhost message)
+  "Shows a growl notification, when user's nick was mentioned.
+If the buffer is currently not visible, makes it sticky."
+  (when (erc-match-current-nick-p nickuserhost message)
+    (let ((s (concat "ERC: " (buffer-name (current-buffer)))))
+      (case system-type
+        ((darwin)
+         (xwl-growl s message))
+        ((gnu/linux)
+         (xwl-shell-command-asynchronously
+          (format "zenity --info --text \"%s\"" s)))))))
+
+(add-hook 'erc-text-matched-hook 'xwl-erc-text-matched-hook)
+
+(defun xwl-erc-PRIVMSG (proc parsed)
+  (let ((buf (buffer-name (current-buffer))))
+    (unless (string-match ":\\|&" buf)
+      (let ((s (concat "ERC: " (buffer-name (current-buffer)))))
+        (case system-type
+          ((darwin)
+           (xwl-growl s message))
+          ((gnu/linux)
+           (xwl-shell-command-asynchronously
+            (format "zenity --info --text \"%s\"" s))))))))
+
+(add-hook 'erc-server-PRIVMSG-functions 'xwl-erc-PRIVMSG)
+
+;; ,----
+;; | misc
+;; `----
+
 (require 'erc-goodies)
 (erc-readonly-mode 1)
 (erc-smiley-mode 1)
@@ -297,35 +318,16 @@ so as to keep an eye on work when necessarily."
           (setq sv (xwl-redirect-host))
           (setq nick "xwl_"))
 
-        (erc-select :server sv :port 16667 :nick nick :password pwerc) 
-        (erc-select :server sv :port 16669 :nick nick :password pwdeb) 
+        (erc-select :server sv :port 16667 :nick nick :password pwerc)
+        (erc-select :server sv :port 16669 :nick nick :password pwdeb)
         (erc-select :server sv :port 16668 :nick nick :password pwerc))
 
-    (erc-select :server "irc.debian.org" ; "localhost"
-                :port 6669               ; 16669
-                :nick "xwl"
-                :password pwdeb)
+    (erc-select :server "irc.debian.org"       :port 6669 :nick "xwl" :password pwdeb)
+    (erc-select :server "irc.freenode.net"     :port 6667 :nick "xwl" :password pwerc)
 
-    (erc-select :server "irc.freenode.net" ; "localhost"
-                :port 6667                 ; 16667
-                :nick "xwl"
-                :password pwerc)
-
-;;   (erc-select :server "irc.linuxfire.com.cn"
-;;               :port 6667
-;;               :nick "xwl"
-;;               :password "")
-
-;;;   (erc-select :server "irc.mozilla.org"
-;;; 	      :port 6667
-;;; 	      :nick "xwl"
-;;; 	      :password "")
-
-;;   (erc-select :server "nsx-r"
-;; 	      :port 6667
-;; 	      :nick "william"
-;; 	      :password "")
-  ))
+    ;; (erc-select :server "irc.linuxfire.com.cn" :port 6667 :nick "xwl" :password "")
+    ;; (erc-select :server "irc.mozilla.org"      :port 6667 :nick "xwl" :password "")
+    ))
 
 (defun xwl-erc-select-bitlbee ()
   (interactive)
@@ -344,81 +346,21 @@ so as to keep an eye on work when necessarily."
 
 (add-hook 'erc-mode-hook (lambda () (abbrev-mode 1)))
 
-;; auto identify
-(defun xwl-erc-auto-identify (server nick)
-  (unless (string-match "localhost" server) ; bitlbee
-    (erc-message "PRIVMSG"
-                 (format "NickServ identify %s" pwbitlbee))))
+;; spelling, TODO, fix this.
+;; (require 'erc-spelling)
+;; (erc-spelling-mode -1)
 
-(add-hook 'erc-after-connect 'xwl-erc-auto-identify)
+;; ;; sound
+;; (erc-sound-enable)
+;; (setq erc-sound-path "/home/william/music/sound")
+;; (setq erc-default-sound "/home/william/music/sound/reflection.mp4")
+;; (setq erc-play-command "mplayer")
 
-;; auto identify bitblee
-(defun his-bitlbee-identify ()
-   "If we're on the bitlbee server, send the identify command to the
- &bitlbee channel."
-   (when (and (string= "localhost" erc-session-server)
-              (string= "&bitlbee" (buffer-name)))
-     (erc-message "PRIVMSG" (format "%s identify %s"
-                                    (erc-default-target)
-                                    pwbitlbee))))
-
-(add-hook 'erc-join-hook 'his-bitlbee-identify)
-
-;; auto op
-(defun xwl-erc-auto-op ()
-  (let ((b (buffer-name)))
-    (cond
-     ((string= b "#emacs-cn")
-      (erc-message "PRIVMSG" (concat "chanserv op " b)))
-     ((string= b "#avkon")
-      (erc-message "PRIVMSG" (concat "userserv login xwl " pwerc))
-      (erc-message "PRIVMSG" (concat "chanserv op " b))))))
-
-(add-hook 'erc-join-hook 'xwl-erc-auto-op)
-
-(when (xwl-tty-p)
-  (global-set-key (kbd "C-c RET") (kbd "C-c C-@")))
-
-
-;; ,----
-;; | growl
-;; `----
-
-(setq growlnotify-command (executable-find "growlnotify"))
-
-(defun xwl-growl (title message)
-  (start-process "growl" " growl" growlnotify-command title "-a" "Emacs")
-  (process-send-string " growl" message)
-  (process-send-string " growl" "\n")
-  (process-send-eof " growl"))
-
-(defun xwl-erc-text-matched-hook (match-type nickuserhost message)
-  "Shows a growl notification, when user's nick was mentioned.
-If the buffer is currently not visible, makes it sticky."
-  (when (erc-match-current-nick-p nickuserhost message)
-    (let ((s (concat "ERC: " (buffer-name (current-buffer)))))
-      (case system-type
-        ((darwin)
-         (xwl-growl s message))
-        ((gnu/linux)
-         (xwl-shell-command-asynchronously 
-          (format "zenity --info --text \"%s\"" s)))))))
-
-(add-hook 'erc-text-matched-hook 'xwl-erc-text-matched-hook)
-
-(defun xwl-erc-PRIVMSG (proc parsed)
-  (let ((buf (buffer-name (current-buffer))))
-    (unless (string-match ":\\|&" buf)
-      (let ((s (concat "ERC: " (buffer-name (current-buffer)))))
-        (case system-type
-          ((darwin)
-           (xwl-growl s message))
-          ((gnu/linux)
-           (xwl-shell-command-asynchronously 
-            (format "zenity --info --text \"%s\"" s))))))))
-
-(add-hook 'erc-server-PRIVMSG-functions 'xwl-erc-PRIVMSG)
-
+;; FIXME
+;; bbdb
+;; (require 'erc-bbdb)
+;; (erc-bbdb-mode 1)
+;; (setq erc-bbdb-popup-type nil)
 
 (provide 'xwl-erc)
 
