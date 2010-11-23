e214879130fc4cb68187354607dc6060f80d5749
William Xu <william.xwl@gmail.com>
2010-11-23 16:53:27 +0800

    which.bat: New file.

---
+++ w32/which.bat
@@ -0,0 +1,22 @@
+rem Downloaded from google.
+
+@echo off
+@set OLDPATH=%PATH%
+@set PATH=.;%PATH%
+
+@rem 
+@rem about:  something similar like the unix-alike-which, but with
+@rem         within pure cmd
+@rem 
+
+if "%1" == "" (
+    @echo Usage: 
+    @echo.
+    @echo   which 'cmd'
+    @echo.
+    @echo.if 'cmd' is not found, ERRORLEVEL is set to 1
+    @echo.  
+) else (
+    ( @for %%f in (%1 %1.exe %1.cmd %1.bat %1.pif) do if not "%%~$PATH:f" == "" ( @echo %%~$PATH:f ) else @set ERRORLEVEL=1) 
+)
+@set PATH=%OLDPATH%
